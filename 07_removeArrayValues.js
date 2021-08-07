e923550a17f6dc3499db6056856f4e6b602752dc
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-08-07 15:52:06 +0900

    removeArrayValues

---
+++ 07_removeArrayValues.js
@@ -0,0 +1,13 @@
+/**
+ * 문제) 객체를 입력받아 배열을 값으로 갖는 속성을 모두 제거해야 합니다.
+ *입력) 인자1: obj (임의의 속성을 갖는 객체)
+ *출력) 별도의 리턴문(return statement)을 작성하지 않습니다.
+ **/
+function removeArrayValues(obj) {
+  // TODO: 여기에 코드를 작성합니다.
+  for (let property in obj) {
+    if (Array.isArray(obj[property])) {
+      delete obj[property];
+    }
+  }
+}
