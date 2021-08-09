451ee6b6c90a5c9e36f9ab899900650ef3575f45
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-08-09 16:20:35 +0900

    isPersonOldEnoughToVote

---
+++ Object/09_isPersonOldEnoughToVote.js
@@ -0,0 +1,14 @@
+/** 
+    문제) 객체를 입력받아 객체가 가진 age 속성값이 합법적으로 투표할 수 있는 나이(18세 이상)인지 여부를 리턴해야 합니다.
+    입력) 인자1: person (age 속성을 갖는 객체) 
+    출력) boolean 타입을 리턴해야 합니다.
+*/
+
+function isPersonOldEnoughToVote(person) {
+  // TODO: 여기에 코드를 작성합니다.
+  if (person.age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
