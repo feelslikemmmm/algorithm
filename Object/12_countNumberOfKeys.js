61965300a17930ce0428a0fe20be4d8455963624
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-08-12 16:24:33 +0900

    countNumberOfKeys

---
+++ Object/12_countNumberOfKeys.js
@@ -0,0 +1,9 @@
+/*
+  문제) 객체를 입력받아 속성의 개수를 리턴해야 합니다.
+  입력) 인자1: obj(임의의 속성을 갖는 객체)
+  출력) number타입을 리턴해야 합니다.
+*/
+function countNumberOfKeys(obj) {
+  // TODO: 여기에 코드를 작성합니다.
+  return Object.keys(obj).length;
+}
