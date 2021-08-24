95b407d90974eecb9b31fdb0a8466082cf405d8b
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-08-24 16:35:39 +0900

    getLastElement

---
+++ Array/03_getLastElement.js
@@ -0,0 +1,14 @@
+/*
+  문제) 배열를 입력받아 배열의 마지막 요소를 리턴해야 합니다.
+  입력) 인자1 : arr 임의의 요소를 갖는 배열 (arr.length >= 0)
+  출력) 배열의 요소를 리턴해야 합니다.
+  주의 사항) 빈 배열을 입력받은 경우, undefined를 리턴해야 합니다.
+*/
+function getLastElement(arr) {
+  // TODO: 여기에 코드를 작성합니다.
+  if (arr.length === 0) {
+    return undefined;
+  } else {
+    return arr.slice(-1)[0];
+  }
+}
